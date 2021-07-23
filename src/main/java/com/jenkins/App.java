dcfef407e2db4d1535c96178794e27bc69e1017b
Rajeev Loghade <69763443+rajeevloghade@users.noreply.github.com>
2021-07-23 18:00:39 +0530

    Update App.java

--- src/main/java/com/jenkins/App.java
+++ src/main/java/com/jenkins/App.java
@@ -11,5 +11,6 @@ public class App {
     public static void main(String[] args) {        
         System.out.println(new App().getGreeting());
         System.out.println("Springboot started suceesfully...");
+        System.out.println("Jenkins Testing");
     }
 }
